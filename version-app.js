85f4ad4b66ecc151cbcdb61561461075053cadcc
rcbus <cleiton.rce@gmail.com>
2020-08-06 20:41:38 -0300

    update_cleiton_20200806_203706

--- version-app.js
+++ version-app.js
@@ -8,11 +8,12 @@ module.exports = {
 }
 
 const appName = 'RCBUS NLW - 2 - MOBILE'
-const version = '1.0.2'
+const version = '1.0.3'
 
 /*
 HISTORICO
 
+06/08/2020 AS 20:41:37 - 1.0.3 - BRANCH: update_cleiton_20200806_203706
 06/08/2020 AS 20:36:03 - 1.0.2 - BRANCH: update_cleiton_20200806_203221
 06/08/2020 AS 20:24:52 - 1.0.1 - BRANCH: update_cleiton_20200806_200844
 06/08/2020 - 1.0.0
